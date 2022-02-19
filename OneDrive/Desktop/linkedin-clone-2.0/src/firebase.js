852eaefa0894f29e7a816cee46903d2eac868d32
dan sidiki <dansidiki@live.com>
2022-02-18 23:55:52 -0600

    last commit

---
+++ OneDrive/Desktop/linkedin-clone-2.0/src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from "firebase"
+import "firebase/auth";
+const firebaseConfig = {
+  apiKey: "AIzaSyD87J_stiVZKOrjej_5rlZtTwfmPgNP7FU",
+  authDomain: "linkedin-clone-def29.firebaseapp.com",
+  projectId: "linkedin-clone-def29",
+  storageBucket: "linkedin-clone-def29.appspot.com",
+  messagingSenderId: "225434714651",
+  appId: "1:225434714651:web:07867299105aaecf476ebe"
+};
+
+//This special line of code here connects everything 
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+//gets the firestore database
+const db = firebaseApp.firestore();
+
+
+export { db };
